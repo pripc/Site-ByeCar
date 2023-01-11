7da1427beeb8de4df83befd38390c0bcaadb10d8
pripc <117996744+pripc@users.noreply.github.com>
2023-01-11 16:45:08 -0300

    Adding Carousel feature

--- JS.js
+++ JS.js
@@ -1,5 +1,29 @@
+
+setInterval(AutomaticCarousel, 50000);
 let index = 0;
-const carouselItens = document.querySelectorAll("#moto")
+const carouselItens = document.querySelectorAll("#moto") //seleciona uma array de elementos que fazem parte do carrosel//
+const MoreInformation = document.querySelectorAll(".cardInformation")
+
+function showCard(){
+if (carouselItens[index].style.visibility = "visible"){
+    MoreInformation[index].style.display = "block";
+}
+}
+
+function closeWindow(index){
+    MoreInformation[index].style.display="none";
+}
+
+function AutomaticCarousel(){
+    if(index < carouselItens.length - 1){
+        index++
+        carouselItens[index].scrollIntoView({ behavior: "smooth" })
+    }if (index = carouselItens.length -1){
+        index = 0
+        carouselItens[index].scrollIntoView({ behavior: "smooth" })
+    }
+}
+
 function goNext(){
     if(index === (carouselItens.length) - 1){
         index = 0
@@ -16,3 +40,8 @@ function goBack(){
     }
     carouselItens[index].scrollIntoView({behavior:"smooth"});
 }
+setInterval(AutomaticCarousel,5000)  
+
+
+
+
